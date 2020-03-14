492868ff283ca13e0f435c2f0ee25866708dcbb2
Denis Granovskiy <freelancerer2009@gmail.com>
2020-03-14 23:15:18 +0200

    fix-1

--- Web/Scripts/OpenModalPosition.js
+++ Web/Scripts/OpenModalPosition.js
@@ -17,17 +17,21 @@
         $.get("/Home/EmployeeDetail", function (data) {
             $('#dialogContent').html(data);
             $('#modDialog').modal('show');
-            $('.send').click(function(e){
-                console.log('send');
-                $.ajax ({  
-                    url: '/Home/MainTable',  
-                    contentType: 'application/html; charset=utf-8',  
-                    type: 'GET' ,  
-                    dataType: 'html'  
-                    })  
-                    .done (function (resultData) {  
-                    $('.mainTable').html(resultData);  
-                    })
+            $('form').submit(function(e){
+                e.preventDefault();
+
+                $.post('/Home/AddEmployee', $('form').serialize(), function(data){
+                }).done(function () { 
+                    $.ajax ({  
+                        url: '/Home/MainTable',  
+                        contentType: 'application/html; charset=utf-8',  
+                        type: 'GET' ,  
+                        dataType: 'html'  
+                        })  
+                        .done (function (resultData) {  
+                        $('.mainTable').html(resultData);  
+                        })
+                 });
                 })
             });
     });
